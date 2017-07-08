d3da2660a00db2727b8696f307aa333ebedc0b30
Bruno Mourao <brunolmourao10@gmail.com>
2017-07-08 15:41:08 -0300

    Classes do model

---
+++ frontend/src/domain/store/item.ts
@@ -0,0 +1,4 @@
+export class Item {
+
+    constructor(public name: string, public price: number,public description: string ) {}
+}
---
+++ frontend/src/domain/store/shop.ts
@@ -0,0 +1,4 @@
+export class Shop {
+
+    constructor(public name: string, public adress: string, public phoneNumber: number) {}
+}
