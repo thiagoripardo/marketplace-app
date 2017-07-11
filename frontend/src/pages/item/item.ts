dcd733e4f19aa5e79524c5b58f63532cac441389
Bruno Mourao <brunolmourao10@gmail.com>
2017-07-11 15:34:04 -0300

    Html tela item

--- frontend/src/pages/item/item.ts
+++ frontend/src/pages/item/item.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { Shop } from '../../domain/store/shop';
-
+import {Item} from '../../domain/store/item'
+import {CartPage} from '../../domain/cart/cart'
 /*
   Generated class for the Item page.
 
@@ -14,7 +15,7 @@ import { Shop } from '../../domain/store/shop';
 })
 export class ItemPage {
   public shop: Shop;
-  
+  public item: Item;
 
   constructor(public navParams: NavParams, public navCtrl: NavController) {
     //this.shop = this.navParams.get('shopSelect');
@@ -23,5 +24,7 @@ export class ItemPage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad ItemPage');
   }
-
+  addCart(item){
+    this.navCtrl.push(CartPage,{item:Item});
+  }
 }
