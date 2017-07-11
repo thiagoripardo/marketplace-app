ca3bfeb66bb714ecdf26694a14901b17452a9591
thiagoripardo <tmripardo@gmail.com>
2017-07-11 17:01:49 -0300

    Revert "Html tela item"
    This reverts commit dcd733e4f19aa5e79524c5b58f63532cac441389.

--- frontend/src/pages/item/item.ts
+++ frontend/src/pages/item/item.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { Shop } from '../../domain/store/shop';
-import {Item} from '../../domain/store/item'
-import {CartPage} from '../../domain/cart/cart'
+
 /*
   Generated class for the Item page.
 
@@ -15,7 +14,7 @@ import {CartPage} from '../../domain/cart/cart'
 })
 export class ItemPage {
   public shop: Shop;
-  public item: Item;
+  
 
   constructor(public navParams: NavParams, public navCtrl: NavController) {
     //this.shop = this.navParams.get('shopSelect');
@@ -24,7 +23,5 @@ export class ItemPage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad ItemPage');
   }
-  addCart(item){
-    this.navCtrl.push(CartPage,{item:Item});
-  }
+
 }
