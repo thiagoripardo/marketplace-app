62762b6eea138602d132f3e35bf1df7a6a4bd2dc
NewtonJ <newtonjoaquim12@gmail.com>
2017-07-11 21:59:50 -0300

    feito registro do item

--- frontend/src/pages/item-registration/item-registration.ts
+++ frontend/src/pages/item-registration/item-registration.ts
@@ -1,5 +1,6 @@
+import { ItemService } from './../../providers/item-service';
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, LoadingController, ToastController } from 'ionic-angular';
 
 /*
   Generated class for the ItemRegistration page.
@@ -13,10 +14,46 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class ItemRegistrationPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  loading: any;
+  regData = {ownerstore:'',info:{name:'',description :'',price:''},action:''};
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public loadingCtrl: LoadingController, public toastCtrl: ToastController, public itemService: ItemService) {}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ItemRegistrationPage');
+  }
+
+  doRegisterItem(){
+    this.showLoader();
+     this.regData.action = "add";
+    this.itemService.postOperation(this.regData).then((result) => {
+      this.loading.dismiss();
+      this.navCtrl.pop();
+    }, (err) =>{
+      this.loading.dismiss();
+      this.presentToast(err);
+    });
+  }
+
+  showLoader(){
+    this.loading = this.loadingCtrl.create({
+      content: 'Registering...'
+    })
+  }
+
+  presentToast(msg) {
+    let toast = this.toastCtrl.create({
+      message: msg,
+      duration: 3000,
+      position: 'bottom',
+      dismissOnPageChange: true
+    });
+
+    toast.onDidDismiss(() => {
+      console.log('Dismissed toast');
+    });
+
+    toast.present();
   }
 
 }
